156a0d7e7ece6ccb59f1ef98d1a66a1961fdbf22
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-06-12 21:27:41 +1000

    Add react update

---
+++ src/commands/react/update.ts
@@ -0,0 +1,34 @@
+import { flags, Command } from '@oclif/command'
+import { core, SfdxCommand } from '@salesforce/command'
+
+// Libraries
+const Util = require('util')
+const exec = Util.promisify(require('child_process').exec)
+
+// Update command
+export default class Update extends Command {
+  public static examples = [`$ sfdx react:update`]
+
+  static flags = {
+    help: flags.help({ char: 'h' })
+  }
+
+  // Comment this out if your command does not require an org username
+  protected static requiresUsername = true
+
+  // Comment this out if your command does not support a hub org username
+  protected static supportsDevhubUsername = true
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = false
+
+  public async run(): Promise<any> {
+    // tslint:disable-line:no-any
+    // Run webpack
+    this.log('Building app...')
+    return exec(`yarn build`).then(({ stdout, stderr }) => {
+      this.log(stdout)
+      this.log(stderr)
+    })
+  }
+}
