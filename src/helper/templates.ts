401f43d725a4b3a8834feb03239d1cae096f91c6
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-06-23 13:52:42 +1000

    Add style-loader and css-loader rules

--- src/helper/templates.ts
+++ src/helper/templates.ts
@@ -19,10 +19,13 @@ const getTemplatePackageJsonConfig = (name: string): string =>
     "babel-preset-env": "^1.7.0",
     "babel-preset-react": "^6.24.1",
     "babel-preset-stage-1": "^6.24.1",
+    "css-loader": "^0.28.11",
+    "style-loader": "^0.21.0",
     "webpack": "^4.12.0",
     "webpack-cli": "^3.0.8"
   },
   "dependencies": {
+    "bootstrap": "^4.1.1",
     "react": "^16.4.1",
     "react-dom": "^16.4.1"
   }
@@ -44,6 +47,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader'] // Loads .babelrc
+      },
+      {
+        test:/\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
@@ -61,14 +68,17 @@ ReactDOM.render(<App />, document.getElementById('root'))`
 
 const getTemplateApp = (): string =>
 `import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h1>
-          Hello World
-        </h1>
+      <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple react app for Visualforce.</p>
+        <hr className="my-4"/>
+        <p>It uses Webpack for resource bundling and Bootstrap for styling.</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
       </div>
     )
   }
@@ -77,7 +87,7 @@ class App extends Component {
 export default App`
 
 const getTemplateIndexHtml = (name: string): string =>
-`<apex:page id="${name}" lightningStylesheets="true">
+`<apex:page id="${name}" lightningStylesheets="false">
   <div id="root"></div>
   <script src="/resource/${name}/App.bundle.js" type="text/javascript"></script>
 </apex:page>`
