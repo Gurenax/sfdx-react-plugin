b3686626722b040d0d5895de8cccd57ad03e3cb8
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-07-17 00:15:07 +1000

    Update templates for css and images

--- src/commands/react/create.ts
+++ src/commands/react/create.ts
@@ -6,6 +6,8 @@ import {
   getTemplateWebpackConfig,
   getTemplateIndex,
   getTemplateApp,
+  getTemplateCSS,
+  getTemplateLogo,
   getTemplateIndexHtml,
   getTemplateTestHtml,
   getTemplateResourceMetadata,
@@ -41,54 +43,92 @@ const writeToFiles = (files: File[]): Promise<any> => (
 )
 
 // Update Command
-export default class Create extends Command {
-  public static examples = [`$ sfdx react:create -n HelloWorld`]
+export default class Create extends SfdxCommand {
+  public static examples = [
+    `$ sfdx react:create -n HelloWorld -u OrgName`,
+    `$ sfdx react:create -n HelloWorld -u OrgName --apiversion 43.0`,
+  ]
 
-  public static args = [{ name: 'file' }]
+  // For Usage help text
+  // public static args = [{ name: 'file' }]
 
-  static flags = {
-    help: flags.help({ char: 'h' }),
-    name: flags.string({
-      char: 'n',
-      description: 'name of the app',
-      required: true
-    })
-  }
+  // Option: if using Heroku CLI Command instead of SfdxCommand
+  // static flags = {
+  //   help: flags.help({ char: 'h' }),
+  //   name: flags.string({
+  //     char: 'n',
+  //     description: 'name of the app',
+  //     required: true
+  //   })
+  // }
+
+  protected static flagsConfig = {
+    // flag for help (-h, --help)
+    help : { char: 'h', description: 'display the help texts' },
+    // flag with a value (-n, --name=VALUE)
+    name: { char: 'n', type: 'string', description: 'name of the app' },
+  };
 
   // Comment this out if your command does not require an org username
   protected static requiresUsername = true
 
   // Comment this out if your command does not support a hub org username
-  protected static supportsDevhubUsername = true
+  // protected static supportsDevhubUsername = true
 
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = false
 
   public async run(): Promise<any> {
+    // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
+    const conn = this.org.getConnection();
+
     // tslint:disable-line:no-any
-    const { flags } = this.parse(Create)
-    const name = flags.name
+    // const { flags } = this.parse(Create) // Option: if using Heroku CLI Command instead of SfdxCommand
+    const name = this.flags.name
+    
+    // Get api version
+    const apiVersion = this.flags.apiversion || '43.0'
+
+    // The type we are querying for
+    interface Organization {
+      Name: string;
+      TrialExpirationDate: string;
+    }
+
+    // Query the org
+    const query = 'Select Name, TrialExpirationDate from Organization';
+    const result = await conn.query<Organization>(query);
+
+    // Organization always only returns one result
+    const orgName = result.records[0].Name;
+    const trialExpirationDate = result.records[0].TrialExpirationDate;
 
     // Load templates
     const templateBabelConfig = getTemplateBabelConfig()
     const templatePackageJsonConfig = getTemplatePackageJsonConfig(name)
     const templateWebpackConfig = getTemplateWebpackConfig(name)
     const templateIndex = getTemplateIndex()
-    const templateApp = getTemplateApp()
+    const templateApp = getTemplateApp(name, apiVersion, orgName, trialExpirationDate)
+    const templateCSS = getTemplateCSS()
+    const templateLogo = getTemplateLogo()
     const templateIndexHtml = getTemplateIndexHtml(name)
     const templateTestHtml = getTemplateTestHtml(name)
     const templateResourceMetadata = getTemplateResourceMetadata(name)
-    const templatePageMetadata = getTemplatePageMetadata(name)
+    const templatePageMetadata = getTemplatePageMetadata(name, apiVersion)
 
     // Create key-value pair of filename and file path
     const files:File[] = [
       { key: `package.json`, value: templatePackageJsonConfig },
       { key: `webpack.config.js`, value: templateWebpackConfig },
       { key: `.babelrc`, value: templateBabelConfig },
-      { key: `force-app/main/default/pages/${name}/src/index.js`,
+      { key: `react/${name}/src/index.js`,
         value: templateIndex },
-      { key: `force-app/main/default/pages/${name}/src/components/App.js`,
+      { key: `react/${name}/src/components/App.js`,
         value: templateApp },
+      { key: `react/${name}/src/components/App.css`,
+        value: templateCSS },
+      { key: `react/${name}/src/components/logo.svg`,
+        value: templateLogo },
       { key: `force-app/main/default/pages/${name}.page`,
         value: templateIndexHtml },
       { key: `force-app/main/default/pages/${name}.test.html`,
@@ -100,36 +140,35 @@ export default class Create extends Command {
     ]
 
     return Promise.all([
-      createDirectoryPromise(
-        `force-app/main/default/pages/${name}/src/components`
-      ),
+      createDirectoryPromise(`react/${name}/src/components`),
+      createDirectoryPromise(`force-app/main/default/pages/`),
       createDirectoryPromise(`force-app/main/default/staticresources/${name}`)
     ]).then(() => 
       writeToFiles(files)
         .then(results => {
           // Output file creation
-          this.log('Files created:', JSON.stringify(results))
+          this.ux.log('Files created:', JSON.stringify(results))
         })
         .then(async () => {
           // Install packages
-          this.log('Installing packages...')
+          this.ux.log('Installing packages...')
           try {
             const { stdout, stderr } = await exec(`yarn install`)
-            this.log(stdout)
-            this.log(stderr)
+            this.ux.log(stdout)
+            this.ux.log(stderr)
           } catch(error) {
-            this.log(error.stdout)
+            this.ux.log(error.stdout)
           }
         })
         .then(async () => {
           // Run webpack
-          this.log('Building app...')
+          this.ux.log('Building app...')
           try {
             const { stdout, stderr } = await exec(`yarn build`)
-            this.log(stdout)
-            this.log(stderr)
+            this.ux.log(stdout)
+            this.ux.log(stderr)
           } catch(error) {
-            this.log(error.stdout)
+            this.ux.log(error.stdout)
           }
         }) 
     )
